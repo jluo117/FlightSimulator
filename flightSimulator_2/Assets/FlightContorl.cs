aee8237fe8c7c62b7b3ec807cee12ad05f0701d1
James Luo <luojames52@gmail.com>
2018-05-21 21:51:12 -0700

    roll and yall

--- flightSimulator_2/Assets/FlightContorl.cs
+++ flightSimulator_2/Assets/FlightContorl.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlightContorl : MonoBehaviour {
-   
+    private float buttonRotation = 0f;
+    private float upDown = 0f;
     public float detalRot;
     public GameObject Plane;
     public GameObject Xwing;
     public float acceleration = 0;
     private Vector3 lastPosition = new Vector3(0, 0, 0);
     private Quaternion lastRotation = new Quaternion(0, 0, 0, 0);
+    private string leftKey = "left";
+    private string rightKey = "right";
+    //private string upKey = "up";
+    //private string downKey = "down";
     // Use this for initialization
     void Start () {
        
@@ -39,31 +44,61 @@ public class FlightContorl : MonoBehaviour {
             moveBack = true;
             dpad_move = true;
         }
-        //if ((dpad_move) && (moveForward))
-        //{
-        //    //acceleration += 2;
-        //}
-
-        //if ((dpad_move) && (moveBack))
-        //{
-        //    acceleration += -2;
-        //}
-
-        //Vector3 planePosition = Plane.transform.position;
-        if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+        if (((dpad_move) && (moveForward)) || (Input.GetKey("w")))
         {
+            print("up");
             acceleration += (float)0.1;
         }
-        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+
+        if ((dpad_move) && ((moveBack)) || (Input.GetKey("s")))
         {
+            print("down");
             acceleration += (float)-0.1;
         }
-        Plane.transform.Translate(Vector3.forward * acceleration * Time.deltaTime);
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(Vector3.left * 25 * Time.deltaTime);
+            print("left");
+            buttonRotation += 10;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(Vector3.right * 25 * Time.deltaTime);
+            print("Right");
+            buttonRotation += -10;
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(Vector3.up * 25 * Time.deltaTime);
+            upDown += 10;
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(Vector3.down * 25 * Time.deltaTime);
+            upDown -= 10;
+        }
+        //Vector3 planePosition = Plane.transform.position;
+        //if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+        //{
+        //    acceleration += (float)0.1;
+        //}
+        //if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        //{
+        //    acceleration += (float)-0.1;
+        //}
+       // Plane.transform.Translate(Vector3.forward * acceleration * Time.deltaTime);
         ////Plane.transform.position += deltaPos;
-        this.transform.rotation *= deltaRot;
+       
         //lastPosition = transform.position;
         //lastRotation = transform.rotation;
-        //transform.Translate(Vector3.forward * acceleration * Time.deltaTime);
+        transform.Translate(Vector3.forward * acceleration * Time.deltaTime);
+        this.transform.rotation *= deltaRot;
+
+        Vector3 euler = transform.rotation.eulerAngles;
+        euler.y += buttonRotation;
+        euler.x += upDown;
+        transform.rotation = Quaternion.Euler(euler);
+        //buttonRotation = 0;
     }
     
 }
